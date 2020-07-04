420538caa89031742f1aa6966aad2d5e220f2128
rytr823 <rytr823@gmail.com>
2020-07-04 16:22:54 +0900

    add with_options

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,16 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :nickname, presence: true
+
   validates :password, format: { with: /\A(?=.?[a-z])(?=.?\d)[a-z\d]{6,100}+\z/i, message: 'は半角英数字混合で入力してください。' }
   validates :password_confirmation, format: { with: /\A(?=.?[a-z])(?=.?\d)[a-z\d]{6,100}+\z/i, message: 'は半角英数字混合で入力してください。' }
-  validates :family_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'は全角で入力して下さい。' }
-  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'は全角で入力して下さい。' }
-  validates :family_name_katakana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'はカタカナで入力して下さい。' }
-  validates :last_name_katakana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'はカタカナで入力して下さい。' }
-  validates :birthday, presence: true
+
+  with_options presence: true do |assoc|
+    assoc.validates :nickname
+    assoc.validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'は全角で入力して下さい。' }
+    assoc.validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'は全角で入力して下さい。' }
+    assoc.validates :family_name_katakana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'はカタカナで入力して下さい。' }
+    assoc.validates :last_name_katakana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'はカタカナで入力して下さい。' }
+    assoc.validates :birthday
+  end
 end
