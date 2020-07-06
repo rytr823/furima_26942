2646a02a47cfc547ecbf4cc10ed9a0fcb962e39f
rytr823 <rytr823@gmail.com>
2020-07-06 21:13:55 +0900

    add new and create

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,37 @@ class ItemsController < ApplicationController
   def index
     @items = Item.order('created_at DESC')
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(
+    :name,
+    :image,
+    :introduction,
+    :price,
+    :sales_commission,
+    :sales_profit,
+    :category_id,
+    :status_id,
+    :postage_pay_id,
+    :shipping_area_id,
+    :days_until_shipping_id,
+    )
+  end
+
+
 end
