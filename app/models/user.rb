414230076950a49e7c12e5c17a45f15d524a1c46
rytr823 <rytr823@gmail.com>
2020-07-07 18:51:05 +0900

    edit password validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :password, format: { with: /\A(?=.?[a-z])(?=.?\d)[a-z\d]{6,100}+\z/i, message: 'は半角英数字混合で入力してください。' }
-  validates :password_confirmation, format: { with: /\A(?=.?[a-z])(?=.?\d)[a-z\d]{6,100}+\z/i, message: 'は半角英数字混合で入力してください。' }
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}\z/i, message: 'は半角英数字混合で入力してください。' }
+  validates :password_confirmation, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}\z/i, message: 'は半角英数字混合で入力してください。' }
 
   with_options presence: true do |assoc|
     assoc.validates :nickname
