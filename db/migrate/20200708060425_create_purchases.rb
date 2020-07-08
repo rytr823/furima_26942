a278db43ef495577845e831b2a9648a974774219
rytr823 <rytr823@gmail.com>
2020-07-08 18:30:45 +0900

    create purchases table

--- db/migrate/20200708060425_create_purchases.rb
+++ db/migrate/20200708060425_create_purchases.rb
@@ -1,8 +1,8 @@
 class CreatePurchases < ActiveRecord::Migration[6.0]
   def change
     create_table :purchases do |t|
-      t.references :user_id, null: false, foreign_key: true
-      t.references :item_id, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
+      t.references :item, null: false, foreign_key: true
       t.timestamps
     end
   end
