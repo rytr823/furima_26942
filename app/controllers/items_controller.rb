c67012a78ffb55f988c55097a9533f2227844f5a
rytr823 <rytr823@gmail.com>
2020-07-09 19:11:07 +0900

    add  edit and update at item

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -21,6 +21,20 @@ class ItemsController < ApplicationController
   def show
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(@item.id)
+    else
+      render :edit
+    end
+  end
+
+
   def set_item
     @item = Item.find(params[:id])
   end
