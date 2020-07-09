6f8cc3e1292c08ae09e181a1d6911386980c5dc7
rytr823 <rytr823@gmail.com>
2020-07-09 14:25:41 +0900

    delete comment out

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -9,13 +9,11 @@ RSpec.describe Item, type: :model do
 
     # 全てのカラムが存在すれば登録できること
     it 'is valid with all column' do
-      # item = build(:item)
       expect(@item).to be_valid
     end
 
     # imageが空であれば登録できないこと
     it 'is invalid without a image' do
-      # item = build(:item)
       @item.image = nil
       @item.valid?
       expect(@item.errors[:image]).to include("can't be blank")
@@ -23,7 +21,6 @@ RSpec.describe Item, type: :model do
 
     # nameが空であれば登録できないこと
     it 'is invalid without a name' do
-      # item = build(:item, name: nil)
       @item.name = nil
       @item.valid?
       expect(@item.errors[:name]).to include("can't be blank")
@@ -31,7 +28,6 @@ RSpec.describe Item, type: :model do
 
     # introductionが空であれば登録できないこと
     it 'is invalid without a introduction' do
-      # item = build(:item, introduction: nil)
       @item.introduction = nil
       @item.valid?
       expect(@item.errors[:introduction]).to include("can't be blank")
@@ -39,7 +35,6 @@ RSpec.describe Item, type: :model do
 
     # priceが空であれば登録できないこと
     it 'is invalid without a price' do
-      # item = build(:item, price: nil)
       @item.price = nil
       @item.valid?
       expect(@item.errors[:price]).to include("can't be blank")
@@ -47,7 +42,6 @@ RSpec.describe Item, type: :model do
 
     # sales_commissionが空であれば登録できないこと
     it 'is invalid without a sales_commission' do
-      # item = build(:item, sales_commission: nil)
       @item.sales_commission = nil
       @item.valid?
       expect(@item.errors[:sales_commission]).to include("can't be blank")
@@ -55,7 +49,6 @@ RSpec.describe Item, type: :model do
 
     # sales_profitが空であれば登録できないこと
     it 'is invalid without a sales_profit' do
-      # item = build(:item, sales_profit: nil)
       @item.sales_profit = nil
       @item.valid?
       expect(@item.errors[:sales_profit]).to include("can't be blank")
@@ -63,7 +56,6 @@ RSpec.describe Item, type: :model do
 
     # category_idが空であれば登録できないこと
     it 'is invalid without a category_id' do
-      # item = build(:item, category_id: nil)
       @item.category_id = nil
       @item.valid?
       expect(@item.errors[:category_id]).to include("can't be blank")
@@ -71,7 +63,6 @@ RSpec.describe Item, type: :model do
 
     # status_idが空であれば登録できないこと
     it 'is invalid without a status_id' do
-      # item = build(:item, status_id: nil)
       @item.status_id = nil
       @item.valid?
       expect(@item.errors[:status_id]).to include("can't be blank")
@@ -79,7 +70,6 @@ RSpec.describe Item, type: :model do
 
     # postage_pay_idが空であれば登録できないこと
     it 'is invalid without a postage_pay_id' do
-      # item = build(:item, postage_pay_id: nil)
       @item.postage_pay_id = nil
       @item.valid?
       expect(@item.errors[:postage_pay_id]).to include("can't be blank")
@@ -87,7 +77,6 @@ RSpec.describe Item, type: :model do
 
     # shipping_area_idが空であれば登録できないこと
     it 'is invalid without a shipping_area_id' do
-      # item = build(:item, shipping_area_id: nil)
       @item.shipping_area_id = nil
       @item.valid?
       expect(@item.errors[:shipping_area_id]).to include("can't be blank")
@@ -95,11 +84,7 @@ RSpec.describe Item, type: :model do
 
     # days_until_shipping_idが空であれば登録できないこと
     it 'is invalid without a days_until_shipping_id' do
-      # item = build(:item, days_until_shipping_id: nil)
       @item.days_until_shipping_id = nil
-
-      # binding.pry
-
       @item.valid?
       expect(@item.errors[:days_until_shipping_id]).to include("can't be blank")
     end
@@ -107,24 +92,18 @@ RSpec.describe Item, type: :model do
     # userが空であれば登録できないこと
     it 'is invalid without a user_id' do
       @item.user = nil
-
       @item.valid?
       expect(@item.errors[:user]).to include('must exist')
     end
 
     # priceの範囲が、¥300~¥9,999,999の間でなければ登録できること
     it 'is valid with the price range is between ¥300 and ¥9,999,999' do
-      # item = build(:item, price: '1000')
       @item.price = '1000'
-
-      # binding.pry
-
       expect(@item).to be_valid
     end
 
     # priceの範囲が、¥300~¥9,999,999の間でなければ登録できないこと
     it 'is invalid without the price range is between ¥300 and ¥9,999,999' do
-      # item = build(:item, price: '10')
       @item.price = '10'
       @item.valid?
       expect(@item.errors[:price]).to include('is not included in the list')
