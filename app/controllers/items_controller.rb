9b41adc4f6369bf093ff17b5b2e28559ca7dfd26
rytr823 <rytr823@gmail.com>
2020-07-09 23:04:45 +0900

    add item delete function

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :set_item, only: [:show, :update, :edit]
+  before_action :set_item, only: [:show, :update, :edit, :destroy]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -30,6 +30,11 @@ class ItemsController < ApplicationController
     else
       render :edit
     end
+  end
+
+  def destroy
+    @item.destroy
+    redirect_to root_path
   end
 
   def set_item
