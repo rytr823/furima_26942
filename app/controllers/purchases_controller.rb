85d94d79807610841eaf5fbccff74995fbe47fe9
rytr823 <rytr823@gmail.com>
2020-07-11 19:30:18 +0900

    Link to Item purchase page

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,5 +1,27 @@
 class PurchasesController < ApplicationController
+  before_action :item
+  before_action :move_toppage
+  before_action :soldout
+
   def index
-    @buyer = Purchase.find(params[:id])
+    redirect_to user_session_path unless user_signed_in?
+  end
+  
+  private
+
+  def move_toppage
+    redirect_to root_path if current_user.id == @item.user_id
+  end
+
+  def item
+    @item = Item.find(params[:item_id])
+  end
+
+  def soldout
+    @purchase = Purchase.find(params[:item_id])
+    redirect_to root_path unless @purchase.nil?
   end
 end
+
+
+# http://localhost:3000/items/1/purchases
