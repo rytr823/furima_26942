520022275079907b9c9f5128773461dedee88c5e
rytr823 <rytr823@gmail.com>
2020-07-15 12:02:53 +0900

    edit README.md

--- db/migrate/20200711071029_create_cards.rb
+++ db/migrate/20200711071029_create_cards.rb
@@ -2,7 +2,7 @@ class CreateCards < ActiveRecord::Migration[6.0]
   def change
     create_table :cards do |t|
       t.string :card_token,     null: false
-      t.references :item,       foreign_key: true
+      t.references :item,       null: false, foreign_key: true
 
       t.timestamps
     end
